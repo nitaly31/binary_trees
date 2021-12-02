353bb5fce9467e1648b969302878637531c876e8
luffy981 <jhonsmith981@hotmail.com>
2021-12-02 17:26:58 -0500

    unction that finds the uncle of a node

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - function that finds the uncle of a node
+ *
+ * @node: Pointer to the tree
+ * Return: Return the height of the tree
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *uncle = node;
+
+	if (!uncle || !(uncle->parent) || !(uncle->parent->parent))
+		return (NULL);
+	if (uncle->parent->parent->left && uncle->parent->parent->left
+			!= node->parent)
+		uncle = uncle->parent->parent->left;
+	else
+		uncle = uncle->parent->parent->right;
+	return (uncle);
+}
