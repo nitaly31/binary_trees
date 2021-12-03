9d56dc9826efe97b7d08bcfca69eea896d8be447
luffy981 <jhonsmith981@hotmail.com>
2021-12-02 23:54:03 -0500

    header file

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
+
 /**
- * create_node - function that finds the sibling of a node
+ * create_node - Creates a new node and sets its parento to parent
+ * @parent: parent of the new node
+ * @value: Value for the new node
  *
- * @parent: val
- * @value: val
- * Return: Return the height of the tree
+ * Return: Pointer to the newly created node
  */
 heap_t *create_node(heap_t *parent, int value)
 {
@@ -13,57 +14,66 @@ heap_t *create_node(heap_t *parent, int value)
 	new_node = malloc(sizeof(*new_node));
 	if (!new_node)
 		return (NULL);
+
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
 	new_node->n = value;
+
 	return (new_node);
 }
+
 /**
- * count_nodes - function that finds the sibling of a node
+ * count_nodes - Counts the number of nodes inside a tree
+ * @root: Pointer to tree's root node
  *
- * @root: val
- * Return: Return the height of the tree
+ * Return: Number of tree nodes
  */
 int count_nodes(binary_tree_t *root)
 {
 	if (!root)
 		return (0);
+
 	return (1 + count_nodes(root->left) + count_nodes(root->right));
 }
+
 /**
- * bubble_up - function that finds the sibling of a node
+ * bubble_up - Puts the newlly created value into it's correct position
+ * @node: Pointer to newly created node
  *
- * @node: val
- * Return: Return the height of the tree
+ * Return: Node's last position
  */
 heap_t *bubble_up(heap_t *node)
 {
-	int *temp;
+	int temp;
 	heap_t *parent;
 
 	if (!node)
 		return (NULL);
+
 	parent = node->parent;
 	while (parent && parent->n < node->n)
 	{
 		temp = parent->n;
 		parent->n = node->n;
 		node->n = temp;
+
 		node = parent;
 		parent = parent->parent;
 	}
+
 	return (node);
 }
+
 /**
- * insert_node - function that finds the sibling of a node
+ * insert_node - Inserts a new node in the heap
+ * @root: Pointer to heap's root node
+ * @size: Number of nodes inside the heap
+ * @index: Index of the current node been evaluated
+ * @pind: New node's parent index
+ * @value: Value of the new node to be created
  *
- * @root: val
- * @size: size
- * @index: index
- * @pind: pind
- * @value: val
- * Return: Return the height of the tree
+ * Return: 1 if the tree is a heap, 0 otherwise
  */
 heap_t *insert_node(heap_t *root, int size, int index, int pind, int value)
 {
@@ -71,6 +81,7 @@ heap_t *insert_node(heap_t *root, int size, int index, int pind, int value)
 
 	if (!root || index > pind)
 		return (NULL);
+
 	if (index == pind)
 	{
 		new_node = create_node(root, value);
@@ -78,22 +89,28 @@ heap_t *insert_node(heap_t *root, int size, int index, int pind, int value)
 			root->left = new_node;
 		else
 			root->right = new_node;
+
 		return (bubble_up(new_node));
 	}
+
 	left = insert_node(root->left, size, index * 2 + 1, pind, value);
 	if (left)
 		return (left);
+
 	right = insert_node(root->right, size, index * 2 + 2, pind, value);
 	if (right)
 		return (right);
+
 	return (NULL);
+
 }
+
 /**
- * heap_insert - function that finds the sibling of a node
+ * heap_insert - Inserts a node in the heap
+ * @root: Double pointer to heap's root node
+ * @value: Value for the new node
  *
- * @root: val
- * @value: val
- * Return: Return the height of the tree
+ * Return: Pointer to the newly created node
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
@@ -101,8 +118,10 @@ heap_t *heap_insert(heap_t **root, int value)
 
 	if (!root)
 		return (NULL);
+
 	if (!(*root))
 		return (*root = create_node(NULL, value));
+
 	nodes = count_nodes(*root);
 	pind = (nodes - 1) / 2;
 
