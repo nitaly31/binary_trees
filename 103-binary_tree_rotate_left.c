3a6f3220758f99e87a4bf51b26559ba2e5ef6845
luffy981 <jhonsmith981@hotmail.com>
2021-12-02 21:36:10 -0500

    function that performs a left-rotation on a binary tree

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_rotate_left - function that finds the sibling of a node
+ *
+ * @tree: Pointer to the tree
+ * Return: Return the height of the tree
+ */
+
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *child;
+	binary_tree_t *parent = tree;
+
+	if (!tree)
+		return (NULL);
+	child = parent->right;
+	if (!child)
+		return (tree);
+	if (child->left)
+		child->left->parent = parent;
+	parent->right = child->left;
+	child->left = parent;
+	child->parent = parent->parent;
+	parent->parent = child;
+	if (child->parent && child->parent->left == parent)
+		child->parent->left = child;
+	else if (child->parent)
+		child->parent->right = child;
+	return (child);
+}
