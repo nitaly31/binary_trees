1998561f37184ec767746ee1affddabda1f2f922
luffy981 <jhonsmith981@hotmail.com>
2021-12-02 22:45:16 -0500

    BST - Search

---
+++ 113-bst_search.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * array_to_bst - function that finds the sibling of a node
+ *
+ * @tree: tree
+ * @value: max
+ * Return: Return the height of the tree
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (!tree)
+		return (NULL);
+	while (tree)
+	{
+		if (value == tree->n)
+			return ((bst_t *)tree);
+		if (value < tree->n)
+			tree = tree->left;
+		else
+			tree = tree->right;
+	}
+	return (NULL);
+}
