468a0025e5d2e83d0c542331ebfddb8b3a04d662
luffy981 <jhonsmith981@hotmail.com>
2021-12-02 22:23:55 -0500

    BST - Insert

---
+++ 111-bst_insert.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+/**
+ * bst_insert - function that finds the sibling of a node
+ *
+ * @value: max
+ * Return: Return the height of the tree
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *prev_parent = NULL;
+	bst_t *parent = NULL;
+	bst_t *new_node = NULL;
+
+	if (!tree)
+		return (NULL);
+	prev_parent = NULL;
+	parent = *tree;
+	while (parent)
+	{
+		prev_parent = parent;
+		if (value == parent->n)
+			return (NULL);
+		if (value < parent->n)
+			parent = parent->left;
+		else
+			parent = parent->right;
+	}
+	new_node = binary_tree_node(prev_parent, value);
+	if (!prev_parent)
+		*tree = new_node;
+	else if (value < prev_parent->n)
+		prev_parent->left = new_node;
+	else
+		prev_parent->right = new_node;
+	return (new_node);
+}
