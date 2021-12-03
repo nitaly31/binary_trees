8def06c8deadf4295e4473ef89da6089d79bfbf2
luffy981 <jhonsmith981@hotmail.com>
2021-12-02 22:32:38 -0500

    BST - Array to BST

---
+++ 112-array_to_bst.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * array_to_bst - function that finds the sibling of a node
+ *
+ * @tree: tree
+ * @value: max
+ * Return: Return the height of the tree
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	size_t i;
+	bst_t *bst = NULL;
+
+	if (!array || size == 0)
+		return (NULL);
+	bst = bst_insert(&bst, array[0]);
+	for (i = 1; i < size; i++)
+		bst_insert(&bst, array[i]);
+	return (bst);
+}
